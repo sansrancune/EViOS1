940df9e0746a40948ed2f74b3233b9d3a98cb87d
ale <peosoalex@hotmail.com>
2021-06-01 13:10:10 +0200

    Initial commit

---
+++ IOS-EVALUATION-01-ALEXTests/IOS_EVALUATION_01_ALEXTests.swift
@@ -0,0 +1,33 @@
+//
+//  IOS_EVALUATION_01_ALEXTests.swift
+//  IOS-EVALUATION-01-ALEXTests
+//
+//  Created by Student00 on 01/06/2021.
+//
+
+import XCTest
+@testable import IOS_EVALUATION_01_ALEX
+
+class IOS_EVALUATION_01_ALEXTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
