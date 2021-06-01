cca3b417fe22d29f162be4d042a6299acf6436da
ale <peosoalex@hotmail.com>
2021-06-01 14:28:56 +0200

    Exo1

--- IOS-EVALUATION-01-ALEX/ViewController.swift
+++ IOS-EVALUATION-01-ALEX/ViewController.swift
@@ -8,12 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var welcomeLabel: UILabel!
+    @IBOutlet weak var avatarImage: UIImageView!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var passwordVisibilityIcon: UIImageView!
+    @IBOutlet weak var newsletterLabel: UILabel!
+    @IBOutlet weak var newsletterSwitch: UISwitch!
+    @IBOutlet weak var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        avatarImage.layer.borderWidth = 1.0
+        avatarImage.layer.masksToBounds = false
+        avatarImage.layer.borderColor = UIColor.white.cgColor
+        avatarImage.layer.cornerRadius = avatarImage.frame.height / 2
+        avatarImage.clipsToBounds = true
+        
+        welcomeLabel.text = "Bienvenue"
+        welcomeLabel.font = .boldSystemFont(ofSize: 20)
+        
+        usernameTextField.placeholder = "Login"
+        passwordTextField.placeholder = "Password"
+        newsletterLabel.text = "Inscription à la newsletter"
+        
+        loginButton.setTitle("Login", for: .normal)
+        loginButton.setTitleColor(.white, for: .normal)
+        loginButton.layer.cornerRadius = 5
+        
+
     }
 
-
+    @IBAction func tapOnLogin(_ sender: Any) {
+    }
+    
 }
 
