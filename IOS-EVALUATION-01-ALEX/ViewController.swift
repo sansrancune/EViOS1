b653fd069f101d01b4f779e991839658b644cb11
ale <peosoalex@hotmail.com>
2021-06-01 15:07:42 +0200

    Exo2

--- IOS-EVALUATION-01-ALEX/ViewController.swift
+++ IOS-EVALUATION-01-ALEX/ViewController.swift
@@ -7,15 +7,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var welcomeLabel: UILabel!
     @IBOutlet weak var avatarImage: UIImageView!
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
-    @IBOutlet weak var passwordVisibilityIcon: UIImageView!
     @IBOutlet weak var newsletterLabel: UILabel!
     @IBOutlet weak var newsletterSwitch: UISwitch!
     @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var passwordVisibilityButton: UIButton!
+    @IBOutlet var openEyeImage = UIImage (named: "eye_on_icon")
+    @IBOutlet var closedEyeImage = UIImage (named: "eye_off_icon")
+    var iconClick = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,9 +41,42 @@ class ViewController: UIViewController {
         loginButton.setTitleColor(.white, for: .normal)
         loginButton.layer.cornerRadius = 5
         
+        passwordVisibilityButton.setBackgroundImage(openEyeImage, for: .normal)
+        passwordTextField.isSecureTextEntry = true
+        
+        usernameTextField.delegate = self
+        passwordTextField.delegate = self
+        
+        let closeKeyboardTap = UITapGestureRecognizer(target: self, action: #selector(closeKeyboard))
+        view.addGestureRecognizer(closeKeyboardTap)
+        
+        usernameTextField.keyboardType = .emailAddress
+        passwordTextField.keyboardType = .URL
 
     }
+    // exit keyboard methods
+    @objc func closeKeyboard() {
+        view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        view.endEditing(true)
+        return true
+    }
 
+    //password visibility + icon management
+    @IBAction func imageTapped(_ sender: Any) {
+    
+        if iconClick == true {
+            passwordTextField.isSecureTextEntry = false
+            passwordVisibilityButton.setBackgroundImage(closedEyeImage, for: .normal)
+        } else {
+            passwordTextField.isSecureTextEntry = true
+            passwordVisibilityButton.setBackgroundImage(openEyeImage, for: .normal)
+        }
+        iconClick = !iconClick
+    }
+    
     @IBAction func tapOnLogin(_ sender: Any) {
     }
     
